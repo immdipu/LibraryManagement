010e71dfcd789b6f12c2cc18860dab3e0de6e333
immdipu <dipuchaurasiya91@gmailcom>
2023-06-09 11:19:52 +0545

    added form validation

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,17 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 
 public class BooksController:Controller{
+
+   static List<Book> books= new List<Book>(){new Book{
+    Id=1, 
+    Title="chemistry",
+    Author="stephen", 
+    Edition="III", 
+    Language="En", 
+    Price=456.5, 
+    PublishedDate=DateTime.Now,
+     Publisher="LPE"}};
     public IActionResult Index(){
-
-List<Book> books= new List<Book>();
-
-books.Add(new Book{Id=1, Title="chemistry",Author="stephen", Edition="III", Language="En", Price=456.5, PublishedDate=DateTime.Now, Publisher="LPE"});
-
         return View(books);
     }
     
     public IActionResult Addbook(){
         return View();
+    }
+    [HttpPost]
+    public IActionResult Addbook(Book book){
+        books.Add(book);
+        return RedirectToAction("Index");
     }
 
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
 public class Book{
     public int Id { get; set; }
+
+    [Required]
+    [MinLength(2)]
     public string Title { get; set; }
     public string Author { get; set; }
     public double Price { get; set; }
+
     public string Publisher { get; set; }
     public string Language { get; set; }
     public string Edition { get; set; }
+
+    [DisplayName("publish Date")]
+    [DataType(DataType.Date)]
     public DateTime PublishedDate { get; set; }
 }
